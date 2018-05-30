e9824d2aaff8666e6b90f04da8061cbcd871877d
Hayden Gill <hjgill7@gmail.com>
2018-05-30 21:33:09 +0100

    hard coded database credentials

--- FMS.DA/DBConnect.cs
+++ FMS.DA/DBConnect.cs
@@ -8,5 +8,32 @@ namespace FMS.DA
 {
     class DBConnect
     {
+        static void Connect(string[] args)
+        {
+            using (SqlConnection connString = new SqlConnection())
+            {
+
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                Connection();
+                void Connection()
+                {
+                    string instance = "sps-soft-hg\\exp_2012";
+                    string dbname = "FMS";
+                    string username = "sa";
+                    string password = "Shohaymi!2";
+
+                    builder["Data Source"] = instance;
+                    builder["Integrated Security"] = false;
+                    builder["Initial Catalog"] = dbname;
+                    builder["User ID"] = username;
+                    builder["Password"] = password;
+
+                    Console.WriteLine(builder.ConnectionString);
+
+                    connString.ConnectionString = builder.ConnectionString;
+                }
+            }
+        }
     }
 }
